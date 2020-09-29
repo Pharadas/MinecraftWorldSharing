9960cc2dd19074c4aa9cacef5304086dc656dd5e
David <davidsudo18@gmail.com>
2020-09-28 22:03:46 -0500

    Added option for changin time delay between syncs

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ import shutil
 import time
 import pathlib
 
-setup()
+with open('Options.txt', 'r') as time:
+    delay_time = int(time.read().split(':')[-1])
 
-time.sleep(5)
+setup()
 
 while 'Minecraft.Windows.exe' in (p.name() for p in psutil.process_iter()):
     print('still running')
     worldSync()
     print(' ')
-    time.sleep(10)
+    time.sleep(delay_time)
+
 print('game closed, ending program')
 
 for i in range(5):
